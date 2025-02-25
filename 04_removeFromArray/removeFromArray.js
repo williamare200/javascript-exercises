8dbf58f95a74ef4d8dffee07d8721eacdfe626c4
william arevalos <williamare200@gmail.com>
2025-02-25 20:27:05 -0300

    feature: function removeFromArray using ...arg
    using ...argument and for loop instead of .includes()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeArray) {
+    return array.filter(value => {
+        // check if value is in removeArray using a for loop
+        for (let index = 0; index < array.length; index++) {
+            if (value === removeArray[index]) {
+                return false;// if value is in removeArray, 
+                //exclude it
+            }
+        }
+    return true //if value is not in removeArray, keep it
+    });
+    
 };
 
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// 

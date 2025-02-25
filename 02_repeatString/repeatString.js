9943e1c972b55bf8f137b8acf8a85ee8c03c110b
william arevalos <williamare200@gmail.com>
2025-02-25 16:22:57 -0300

    feature: function repeatString working and pass all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, timesrepeat) {
+    if(timesrepeat < 0) {
+        return 'ERROR'
+    } else {
+        let stringRepetition = "";
+        for (let index = 0; index < timesrepeat; index++) {
+            stringRepetition += string;       
+    }
+        return stringRepetition;
+    }
 
 };
-
+repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;

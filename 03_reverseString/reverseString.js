4104d5979acbd09500b712f7ae742efb4c835ec4
william arevalos <williamare200@gmail.com>
2025-02-25 18:00:33 -0300

    feature: function  reverseString and pass all test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // split string into array 
+    let arrayString = string.split("")
+    console.log(arrayString);
+    let reverseString = '';
+    // iteration array index 0 to end 
+    for (let index = arrayString.length - 1 ; index >= 0; index--) {
+        // pop reverse all indexes to new variable 
+        reverseString += arrayString.pop();
+    }
+    return reverseString;
 };
 
+reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
+
+// string to array 
+// string to reverse
+// countdown array.lenght -1 to index 0
+
+// solutions 

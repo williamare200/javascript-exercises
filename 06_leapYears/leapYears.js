a412fdeb3592bcb0b7e15711740d041d2a74ce73
william arevalos <williamare200@gmail.com>
2025-02-26 19:11:44 -0300

    feature: function leapYears pass all test
    work with century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+// divisible by 4 
+if (year % 4 === 0 ) {
+    // divisible by 100 and 400
+    if(year % 100 === 0 && year % 400 === 0) {
+        return true
+        // divisible by 100 but not 400
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+        return false
+    } else {
+        return true
+    }
+    
+} else {
+    return false;
+}
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// The year is exactly divisible by four (with no remainder);
+// If the year is divisible by 100 (years ending in two zeros), it is not a leap, except if
+// It is also divisible by 400 (in this case it will be a leap year).

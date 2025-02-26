e63729a49881902e8e07f9d32f3b574e34ccf5aa
william arevalos <williamare200@gmail.com>
2025-02-26 17:39:16 -0300

    feature: function sumAll pass all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+let sumStartFinish;
+const sumAll = function(a, b) {
+    sumStartFinish = 0;
+    let i = a > b && a > 0 ? b : a;
+    let finish = b > a && b > 0 ? b : a;
+    if (a < 0 || b < 0 || Number.isInteger(a) == false || Number.isInteger(b) == false
+|| typeof a !== "number" || typeof b !== "number") {
+        return 'ERROR'
+    }
+    
+    for(i; i <= finish; i++) {
+        sumStartFinish += i;
+    }
+    return sumStartFinish;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
+

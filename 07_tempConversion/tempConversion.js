aa6586c44f3c2ec61833f147fe2dd6a57016c566
william arevalos <williamare200@gmail.com>
2025-02-27 15:58:02 -0300

    feature: function  tempConversion pass 1 test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  // celsius from fahrenheit 
+  let celsiusTemperature = (temperature - 32) * (5 / 9);
+  let rounded1 = Math.round(celsiusTemperature * 10) / 10;
+   return rounded1;
 };
-
-const convertToFahrenheit = function() {
+convertToCelsius(32);
+const convertToFahrenheit = function(temperature) {
+  // fahrenheit from celius
+  let fahrenheitTemperature = (temperature * (9 / 5) + 32);
+  let rounded1 = Math.round(fahrenheitTemperature * 10) / 10;
+    return rounded1;
 };
 
 // Do not edit below this line
